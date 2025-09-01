6dd410c95564b9f7f15c8660ef3445e56089743c
Yahwthani <0246038@up.edu.mx>
2025-08-31 23:07:23 -0600

    Update Main

--- secret-design-website/.next/server/chunks/ssr/src_components_6b39529a._.js
+++ secret-design-website/.next/server/chunks/ssr/src_components_6b39529a._.js
@@ -227,7 +227,7 @@ function Header() {
                         }, this),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("button", {
                             onClick: toggleMenu,
-                            className: "md:hidden flex items-center justify-center w-8 h-8 rounded-md hover:bg-gray-100 transition-colors duration-200",
+                            className: "md:hidden flex items-center justify-center w-16 h-16 rounded-md hover:bg-gray-100 transition-colors duration-200",
                             "aria-label": "Abrir menú",
                             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                 className: "flex flex-col justify-center items-center space-y-1",
